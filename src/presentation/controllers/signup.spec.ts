1bac5eae2337c227219b4f24ac1b41a2bb2b6c5f
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-01-11 10:11:35 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
-  // const name = 'any_name'
+  const name = 'any_name'
   const email = 'any_email@email.com'
   const password = 'any_password'
   const passwordConfirmation = 'any_password'
@@ -18,5 +18,19 @@ describe('SignUp Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missin param: name'))
+  })
+
+  test('Should return 400 if no name is provided', async () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name,
+        password,
+        passwordConfirmation
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missin param: email'))
   })
 })
